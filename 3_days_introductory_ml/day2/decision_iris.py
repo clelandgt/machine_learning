daf6345d55a1657221dc24e3cc845e4608975082
cleland <clelandgt@163.com>
2019-11-11 23:52:48 +0800

    ADD: decision_iris

---
+++ 3_days_introductory_ml/day2/decision_iris.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @File  : decision_iris.py
+# @Author: clelandgt@163.com
+# @Date  : 2019-11-11
+# @Desc  : 使用决策树对鸢尾花进行分类
+
+
+def decision_iris():
+    pass
+
+
+def main():
+    decision_iris()
+
+
+if __name__ == '__main__':
+    main()
