f29d9b5561f7ee1e8224085bae8963d5b9d63be4
clelandgt <clelandgt@163.com>
2020-03-23 11:09:06 +0800

    UPD: linear regression add SGD

--- ml_algorithms/ml_libs/linear_regression.py
+++ ml_algorithms/ml_libs/linear_regression.py
@@ -2,11 +2,11 @@
 # @File  : linear_regression.py
 # @Author: clelandgt@163.com
 # @Date  : 2020-03-22
-# @Desc  : 线性回归实现
+# @Desc  : 线性回归的多种实现
 import numpy as np
 
 
-class LinearRegressionGradientDescentCycle:
+class LinearRegression:
     def __init__(self):
         self._thetas = None
         # 截距
@@ -14,6 +14,20 @@ class LinearRegressionGradientDescentCycle:
         # 参数系数
         self.coefs = None
 
+    def fit(self, x_train, y_train):
+        raise NotImplementedError
+
+    def predict(self, x_predict):
+        raise NotImplementedError
+
+    @staticmethod
+    def score(y, y_predict):
+        """R评价"""
+        return 1 - np.dot(y_predict - y, y_predict - y) / len(y) / np.var(y)
+
+
+class LinearRegressionBGDLoop(LinearRegression):
+    """ 批量梯度下降(BGD)_循环维度 """
     def j(self, x, y, theta):
         """目标函数"""
         try:
@@ -23,7 +37,7 @@ class LinearRegressionGradientDescentCycle:
             return float('inf')
 
     def dj(self, x, y, thetas):
-        """求导"""
+        """求导(梯度)"""
         res = np.empty(len(thetas))
         # 第0个theta,其实就是截距
         res[0] = np.sum(np.dot(x, thetas) - y)
@@ -32,7 +46,7 @@ class LinearRegressionGradientDescentCycle:
 
         return res * 2 / len(x)
 
-    def gradient_descent(self, x, y, initial_thetas, eta=0.01, epsilon=1e-8, max_iters=1e4):
+    def gradient_descent(self, x, y, initial_thetas, eta, epsilon, max_iters):
         """梯度下降"""
         thetas = initial_thetas
         while max_iters > 0:
@@ -50,8 +64,8 @@ class LinearRegressionGradientDescentCycle:
 
     def fit(self, x_train, y_train, eta=0.01, epsilon=1e-8, max_iters=1e4):
         """训练"""
-        # 将x转置加上一列全为1
-        X_b = np.hstack([np.ones((len(x_train), 1)), x_train.reshape(-1, 1)])
+        # 加上一列全为1
+        X_b = np.hstack([np.ones((len(x_train), 1)), x_train])
         initial_thetas = np.zeros(X_b.shape[1])
         self.gradient_descent(X_b, y_train, initial_thetas, eta, epsilon, max_iters)
 
@@ -66,22 +80,80 @@ class LinearRegressionGradientDescentCycle:
         return 1 - np.dot(y_predict - y, y_predict - y) / len(y) / np.var(y)
 
 
+class LinearRegressionBGDVector(LinearRegressionBGDLoop):
+    """批量梯度下降(BGD)_向量化维度"""
+    def dj(self, x, y, thetas):
+        """向量实现"""
+        return (x.T).dot(x.dot(thetas)-y) * 2 / len(x)
+
+
+class LinearRegressionSGD(LinearRegressionBGDLoop):
+    """随机梯度下降(SDG)"""
+    def dj(self, x_i, y_i, thetas):
+        return (x_i.T).dot(x_i.dot(thetas)-y_i) * 2
+
+    def gradient_descent(self, x, y, initial_thetas, eta, epsilon, max_iters, t0, t1):
+        """梯度下降"""
+        thetas = initial_thetas
+        while max_iters > 0:
+            # 随机抽取一个样本
+            i = np.random.randint(len(x))
+            # 梯度gradient
+            gradient = self.dj(x[i], y[i], thetas)
+            last_thetas = thetas
+            # 学习率模拟退火
+            eta = (t0 + eta) / (max_iters + t1)
+            thetas = thetas - eta * gradient
+            if(abs(self.j(x, y, thetas) - self.j(x, y, last_thetas)) < epsilon):
+                break
+            max_iters -= 1
+
+        self._thetas = thetas
+        self.intercept = thetas[0]
+        self.coefs = thetas[1:]
+
+    def fit(self, x_train, y_train, eta=0.01, epsilon=1e-8, max_iters=1e4, t0=5, t1=50):
+        """训练"""
+        # 加上一列全为1
+        X_b = np.hstack([np.ones((len(x_train), 1)), x_train])
+        initial_thetas = np.zeros(X_b.shape[1])
+        self.gradient_descent(X_b, y_train, initial_thetas, eta, epsilon, max_iters, t0, t1)
+
+
 def main():
 
     # 1. 初始化数据
-    num_size = 200
+    num_size = 2000
     np.random.seed(100)
     # np.random.random(size=num_size) 0-1的随机数；np.random.normal(size=num_size) 正态分布
     x = np.random.random(size=num_size)
     # 函数 y=10x+5 然后设置一定的随机波动
     y = 10 * x + 5 + np.random.normal(size=num_size)
+    x = x.reshape(-1, 1)
 
-    linear = LinearRegressionGradientDescentCycle()
-    linear.fit(x, y)
-    predict_y = linear.predict(x)
-    print('coefs: ', linear.coefs)
-    print('intercept: ', linear.intercept)
-    print('score: ', linear.score(y, predict_y))
+    lin_reg = LinearRegressionBGDLoop()
+    lin_reg.fit(x, y)
+    predict_y = lin_reg.predict(x)
+    print('---- BGD Loop ----')
+    print('coefs: ', lin_reg.coefs)
+    print('intercept: ', lin_reg.intercept)
+    print('score: ', lin_reg.score(y, predict_y))
+
+    lin_reg = LinearRegressionBGDVector()
+    lin_reg.fit(x, y)
+    predict_y = lin_reg.predict(x)
+    print('---- BGD Vector ----')
+    print('coefs: ', lin_reg.coefs)
+    print('intercept: ', lin_reg.intercept)
+    print('score: ', lin_reg.score(y, predict_y))
+
+    lin_reg = LinearRegressionSGD()
+    lin_reg.fit(x, y)
+    predict_y = lin_reg.predict(x)
+    print('---- SGD ----')
+    print('coefs: ', lin_reg.coefs)
+    print('intercept: ', lin_reg.intercept)
+    print('score: ', lin_reg.score(y, predict_y))
 
 
 if __name__ == '__main__':
